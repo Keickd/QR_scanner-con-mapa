17bcfd5eba468719f423f14a10a4ab255b5db2cf
Marcos Salas <keickd16@gmail.com>
2022-08-28 23:04:06 +0200

    bottomnavigationbar and floatingactionbutton

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scanner/widgets/widgets.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Historial'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.delete_forever),
+          )
+        ],
+      ),
       body: Center(
         child: Text('Home PAge'),
       ),
+      bottomNavigationBar: const CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
