ba8097478ce784a916e10419b5837f2d438b4c33
Marcos Salas <keickd16@gmail.com>
2022-08-28 16:57:40 +0200

    archivos iniciales

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scanner/pages/home_page.dart';
+import 'package:qr_scanner/pages/mapa_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,8 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      title: 'QR Reader',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomePage(),
+        'mapa': (_) => MapaPage(),
+      },
     );
   }
 }
